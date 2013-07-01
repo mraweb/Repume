8d5f98477bec6a3ebc4c6ec1913f36b07b462a6c
Marcelo Almeida <marcelo@mraweb.com.br>
2013-07-01 17:57:18 -0300

    Inicio do Recorte 2013-07-01

--- index.php
+++ index.php
@@ -14,38 +14,167 @@
 
 <div class="bg-home">
     <header role="banner">
-        <h1><a href="/" title="" class="hidetxt"></a></h1>
+        <h1><a href="/" title="Repume" class="hidetxt">Repume</a></h1>
+
+        <div class="telefone">
+            <div id="google_translate_element"></div>
+
+            <p class="tel"><span>55 11</span> 4139-1656</p>
+        </div>
         
         <nav role="navigation">
-            <ul>
+            <ul id="navMenu">
                 <li><a href="/" title="Home">Home</a></li>
-                <li><a href="informacoes.php" title="Informações">Informações</a></li>
                 <li><a href="empresa.php" title="Empresa">Empresa</a></li>
-                <li><a href="cotacao.php" title="Cotação">Cotação</a></li>
-                <li><a href="servicos.php" title="Serviços">Serviços</a></li>
+                <li>
+                    <a href="produtos.php" title="Produtos">Produtos</a>
+                    <ul>
+                        <li><a href="iluminacao-publica.php" title="Iluminação Pública">Iluminação Pública</a></li>
+                        <li><a href="reator.php" title="Reator">Reator</a></li>
+                        <li><a href="iluminacao-industrial.php" title="Iluminação Industrial">Iluminação Industrial</a></li>
+                        <li><a href="portico-estruturas-metalicas.php" title="Portico/Estruturas Metálicas">Portico/Estruturas Metálicas</a></li>
+                        <li><a href="iluminacao-especifica.php" title="Iluminação Específica">Iluminação Específica</a></li>
+                        <li><a href="linha-led-drives.php" title="Linha LED/Drives">Linha LED/Drives</a></li>
+                        <li><a href="iluminacao-decorativa.php" title="Iluminação Decorativa">Iluminação Decorativa</a></li>
+                        <li><a href="lancamentos.php" title="Lançamentos">Lançamentos</a></li>
+                        <li><a href="poste.php" title="Poste">Poste</a></li>
+                    </ul>
+                </li>
+                <li><a href="portfolio.php" title="Portolio">Portolio</a></li>
+                <li><a href="noticias.php" title="Notícias">Notícias</a></li>
+                <li><a href="representantes.php" title="Representantes">Representantes</a></li>
+                <li><a href="clientes.php" title="Clientes">Clientes</a></li>
                 <li><a href="contato.php" title="Contato">Contato</a></li>
             </ul>
         </nav>
     </header><!-- FINAL HEADER -->
     
-    <section id="content" role="main">
-    
-    </section><!-- FINAL CONTENT -->  
+    <div id="content" role="main">
+        <div id="slide-home">
+            <div class="slides_container">
+                <div class="slide">
+                    <img src="img/slide01.jpg" alt="Iluminação Pública" width="527" height="254" />
+                </div>
+
+                <div class="slide">
+                    <img src="img/slide02.jpg" alt="Iluminação Pública" width="527" height="254" />
+                </div>
+
+                <div class="slide">
+                    <img src="img/slide03.jpg" alt="Iluminação Pública" width="527" height="254" />
+                </div>
+
+                <div class="slide">
+                    <img src="img/slide04.jpg" alt="Iluminação Pública" width="527" height="254" />
+                </div>
+
+                <div class="slide">
+                    <img src="img/slide05.jpg" alt="Iluminação Pública" width="527" height="254" />
+                </div>
+
+                <div class="slide">
+                    <img src="img/slide06.jpg" alt="Iluminação Pública" width="527" height="254" />
+                </div>
+            </div><!-- FINAL SLIDES CONTAINER -->
+        </div><!-- FINAL SLIDE-HOME -->
+
+        <section class="lancamentos">
+            <h2>Lançamento REPUME:</h2>
+
+            <img src="img/foto.jpg" alt="Nome do Produto" widht="269" height="152" />
+
+            <a href="#" title="Confira" class="hidetxt">Confira Lançamentos REPUME</a>
+        </section><!-- FINAL LANCAMENTOS -->
+
+        <nav role="navigation">
+            <ul class="produtos">
+                <li>
+                    <img src="img/iluminacao-publica.png" alt="Iluminação Pública" width="225" height="113" />
+
+                    <a href="iluminacao-publica.php" title="Iluminação Pública">Iluminação Pública</a>
+                </li>
+
+                <li>
+                    <img src="img/iluminacao-industrial.png" alt="Iluminação Industrial" width="225" height="113" />
+
+                    <a href="iluminacao-industrial.php" title="Iluminação Industrial">Iluminação Industrial</a>
+                </li>
+
+                <li>
+                    <img src="img/iluminacao-especifica.png" alt="Iluminação Específica" width="225" height="113" />
+
+                    <a href="iluminacao-especifica.php" title="Iluminação Específica">Iluminação Específica</a>
+                </li>
+
+                <li>
+                    <img src="img/iluminacao-decorativa.png" alt="Iluminação Decorativa" width="225" height="113" />
+
+                    <a href="iluminacao-decorativa.php" title="Iluminação Decorativa">Iluminação Decorativa</a>
+                </li>
+
+                <li>
+                    <img src="img/poste.png" alt="Poste/Braço" width="225" height="113" />
+
+                    <a href="poste.php" title="Poste/Braço">Poste/Braço</a>
+                </li>
+
+                <li>
+                    <img src="img/reator.png" alt="Reator" width="225" height="113" />
+
+                    <a href="reator.php" title="Reator">Reator</a>
+                </li>
+
+                <li>
+                    <img src="img/portico-estruturas-metalicas.png" alt="Portico/Estruturas Metálicas" width="225" height="113" />
+
+                    <a href="portico-estruturas-metalicas.php" title="Portico/Estruturas Metálicas">Portico/Estruturas Metálicas</a>
+                </li>
+
+                <li>
+                    <img src="img/linha-led-drives.png" alt="Linha LED/Drivers" width="225" height="113" />
+
+                    <a href="linha-led-drives.php" title="Linha LED/Drivers">Linha LED/Drivers</a>
+                </li>
+            </ul>
+        </nav><!-- FINAL NAV PRODUTOS -->
+
+        <p class="bt-repr"><a href="representantes.php" title="Conheça Nossos Representantes">Conheça Nossos <span>Representantes</span></a></p>
+
+        <img src="img/selo.jpg" alt="30 Anos Repume Iluminação" width="" height="" class="left" />
+
+        <section class="trabalhe">
+            <h3><a href="contato.php" title="Trabalhe Conosco">Trabalhe Conosco</a></h3>
+
+            <p>Confira as últimas vagas</p>
+        </section>
+    </div><!-- FINAL CONTENT -->  
 </div>
 
 <div class="bg-rodape">
     <footer role="contentinfo">
+        <address>
+            <div itemscope itemtype="http://schema.org/Organization">
+                <span itemprop="name" class="logo-footer hidetxt">Repume Iluminação</span>
 
+                <div itemprop="address" itemscope itemtype="http://schema.org/PostalAddress">
+                    <span itemprop="streetAddress">Rua Guilherme de Almeida, 25 - Jardim Saint Moritz</span><br />
+                    <span itemprop="addressLocality">Taboão da Serra</span> - 
+                    <span itemprop="addressRegion">SP</span> - CEP 
+                    <span itemprop="postalCode">06787-440</span>
+                </div>
+            </div>
+        </address>
 
-
-
-
-        
-        <p class="selo"><a href="http://www.ao5.com.br" title="AO5 - Criação de Sites" class="ao5 hidetxt" rel="external">AO5 - Criação de Sites</a></p>
+        <ul>
+            <li class="tel">55 11 4139-1656</li>
+            <li class="fax">55 11 4139-6655</li>
+            <li class="email"><a href="mailto:repume@repume.com.br" title="E-mail" class="link">repume@repume.com.br</a></li>
+        </ul>
     </footer>
 </div>
 
-<script type="text/javascript" src="js/jquery-1.8.2.min.js"></script>
+<script src="http://code.jquery.com/jquery-1.9.1.min.js"></script>
+<script>window.jQuery || document.write('<script src="js/jquery-1.9.1.min.js"><\/script>')</script>
 <script type="text/javascript" src="js/slides.min.jquery.js"></script>
 <script type="text/javascript" src="js/jquery.navMenu.js"></script>
 <script type="text/javascript" src="js/scripts.js"></script>
