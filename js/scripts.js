e47fda4c8f8f82ba8ec74f773b12dfbfe8175c02
Marcelo Almeida <marcelo@mraweb.com.br>
2013-07-01 11:57:18 -0300

    Inicio da Estrutura HTML

---
+++ js/scripts.js
@@ -0,0 +1,134 @@
+$(document).ready(function(){
+	$("a[rel=external]").attr('target','_blank');
+});
+
+jQuery(document).ready(function(){
+    jQuery('head').append('<meta name="SKYPE_TOOLBAR" content="SKYPE_TOOLBAR_PARSER_COMPATIBLE" />')
+});
+
+// Slide home
+$(function(){
+    // Set starting slide to 1
+    var startSlide = 1;
+    // Get slide number if it exists
+    if (window.location.hash) {
+        startSlide = window.location.hash.replace('#','');
+    }
+    // Initialize Slides
+    $('#slide-home').slides({
+        preload: true,
+        preloadImage: 'img/loading.gif',
+        generatePagination: true,
+        play: 5000,
+        pause: 2500,
+        hoverPause: true,
+        // Get the starting slide
+        start: startSlide,
+        effect: 'fade'
+    });
+});
+
+jQuery(document).ready(function() {
+    var menus = jQuery('#navMenu li');
+    menus.on('mouseenter keyup mouseleave keydown',function(e) {
+        clearTimeout($.data(this, 'timer'));
+        if(e.type == 'mouseenter' || e.type == 'keyup'){
+            if(jQuery(this).find('ul').hasClass('sub-nav')){
+                jQuery(this).find('a').eq(0).addClass('current-menu');
+            }
+        $.data(this, 'timer', setTimeout($.proxy(function() {
+            jQuery('.sub-nav', this).stop(true, true).fadeIn('slow');
+        }, this), 100));
+            
+        } else if(e.type == 'mouseleave'){
+            jQuery(this).find('a').eq(0).removeClass('current-menu');
+            jQuery('.sub-nav', this).stop(true, true).fadeOut();
+
+        } else if(e.type == 'keydown'){
+            var totalLi = jQuery(this).find('.sub-nav li');
+            var qtLi = totalLi.length-1;
+            jQuery(this).find('a').eq(0).removeClass('current-menu');
+            jQuery(this).find('.sub-nav li').eq(qtLi).focusout(function(){
+                jQuery('.sub-nav').stop(true, true).fadeOut('slow');
+            });
+        }
+    });
+});
+
+// navegacao entre abas
+$(document).ready(function() {
+    $(".tab_content").hide(); 
+        if(location.hash != "") {
+        var target = "#"+location.hash.split("#")[1]; // need semicolon at end of line
+        $(location.hash).show(); //Show first tab content
+        $("ul.tabs li:has(a[href="+target+"])").addClass("active").show();
+        rotateTabs=false;
+        } else {
+        $("ul.tabs li:eq(0)").addClass("active").show(); //Activate first tab
+        $(".tab_content:eq(0)").show(); //Show first tab content
+    }
+
+    $("ul.tabs li").click(function() {
+        $("ul.tabs li").removeClass("active"); 
+        $(this).addClass("active"); 
+        $(".tab_content").hide(); 
+        var activeTab = $(this).find("a").attr("href"); 
+        $(activeTab).fadeIn();
+        return false
+    });
+});
+
+// Link box-pdr
+$(document).ready(function(){                  
+    $(".slide div, .box-produto, .confira-produto, .lista-produtos li, .noticia").click(function(){
+        window.location=$(this).find("a").attr("href");return false;
+    });
+});
+
+// translate
+function googleTranslateElementInit() {
+    new google.translate.TranslateElement({pageLanguage: 'pt', includedLanguages: 'en,es,pt', layout: google.translate.TranslateElement.InlineLayout.SIMPLE, autoDisplay: false}, 'google_translate_element');
+}
+
+// funcao muda imagem no produtos detalhe
+function mostraFotos(obj){
+    base = document.getElementById("fotos").getElementsByTagName("div")
+    for(i=0;i<base.length;i++){
+        base[i].style.display="none";
+    }
+    document.getElementById(obj).style.display = "block";
+}
+
+function mostraItemSub(obj,group){
+    base = document.getElementById(group).getElementsByTagName("div")
+    for(i=0;i<base.length;i++){
+        base[i].style.display="none";
+    }
+    document.getElementById(obj).style.display = "block";
+}
+
+function abreLink(obj,classe,group){
+    base = document.getElementById(group).getElementsByTagName("div")
+    for(i=0;i<base.length;i++){
+        if (base[i].className == classe){
+        base[i].style.display="none";
+        }
+    }
+    document.getElementById(obj).style.display = "block";
+}
+
+/*****************************************************/
+
+function mudaImg(objDestino,novaImg,texto,numeracao){
+    document.getElementById(objDestino).src = novaImg;
+    //document.getElementById("ultimosRealizadosTxt").innerHTML = texto;
+    atual = numeracao;
+    
+    
+}
+
+// select da pag distribuidores
+$('#selecione').change(function(){
+        $('.box-padrao').hide();
+        $('#' + $(this).val()).show();
+ });
